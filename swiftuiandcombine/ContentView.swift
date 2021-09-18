3ed599bb1601476815c6bfab6daacca64eb7d62c
Daniel Araujo <danielaraujos@live.com>
2021-09-18 10:29:59 -0300

    Initial Commit

---
+++ swiftuiandcombine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftuiandcombine
+//
+//  Created by Daniel Araujo on 18/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftuiandcombine/swiftuiandcombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuiandcombineApp.swift
+//  swiftuiandcombine
+//
+//  Created by Daniel Araujo on 18/09/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftuiandcombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

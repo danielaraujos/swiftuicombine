525b82b6c625dac4e86d6e3add68d76d65c65ef7
Daniel Araujo <danielaraujos@live.com>
2021-09-20 17:44:17 -0300

    :sparkles: cria views iniciais para app

---
+++ swiftuiandcombine/Views/PackagesView.swift
@@ -0,0 +1,20 @@
+//
+//  PackagesView.swift
+//  swiftuiandcombine
+//
+//  Created by Daniel Araujo on 20/09/21.
+//
+
+import SwiftUI
+
+struct PackagesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PackagesView_Previews: PreviewProvider {
+    static var previews: some View {
+        PackagesView()
+    }
+}

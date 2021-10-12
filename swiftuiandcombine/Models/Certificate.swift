872c8e7b9b2081b4ff0bae93d295782b89adf7ae
Daniel Araujo <danielaraujos@live.com>
2021-10-12 14:32:29 -0300

    :sparkles: cria model para os certificados

---
+++ swiftuiandcombine/Models/Certificate.swift
@@ -0,0 +1,16 @@
+//
+//  Certificate.swift
+//  swiftuiandcombine
+//
+//  Created by Daniel Araujo on 12/10/21.
+//
+
+import Foundation
+
+struct Certificate:  Identifiable {
+    var id: String
+    var course: String
+    var date: String
+    var logo: String
+    var instructor: String
+}

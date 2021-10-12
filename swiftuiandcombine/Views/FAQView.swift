2dd503ef4957fe9371efc1283895f94f09680758
Daniel Araujo <danielaraujos@live.com>
2021-10-12 12:46:19 -0300

    :sparkles: cria tela FAQ View

--- swiftuiandcombine/Views/FAQView.swift
+++ swiftuiandcombine/Views/FAQView.swift
@@ -8,9 +8,48 @@
 import SwiftUI
 
 struct FAQView: View {
+    
+    @State private var contentOffset = CGFloat(0)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .top){
+            TrackableScrollView(offsetChanged: { offsetPoint
+                in
+                contentOffset = offsetPoint.y
+            }) {
+                content
+            }
+            
+            VisualEffectBlur(blurStyle: .systemMaterial)
+                .opacity(contentOffset < -16 ? 1 : 0)
+                .animation(.easeIn)
+                .ignoresSafeArea()
+                .frame(height: 0)
+        }
+        .frame(maxHeight: .infinity, alignment: .top)
+        .navigationTitle("FAQ ")
+        .font(.largeTitle)
+        .background(Color("Background 2"))
+        .edgesIgnoringSafeArea(.all)
+            
     }
+    
+    var content: some View {
+        VStack(alignment: .leading, spacing: 16.0){
+            ForEach(faqDataModel, id:\.id) { faq in
+                FAQRow(faq: faq)
+            }
+            
+            Text("Have any question?")
+                .font(.subheadline)
+                .opacity(0.7)
+            
+            //PrimaryButton()
+            
+        }.padding(.horizontal, 20)
+        .padding(.top, 160)
+    }
+    
 }
 
 struct FAQView_Previews: PreviewProvider {

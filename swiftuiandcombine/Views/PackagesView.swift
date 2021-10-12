1b015d8e68984077cbcd73a78369fad1b7cb3e05
Daniel Araujo <danielaraujos@live.com>
2021-10-12 12:46:06 -0300

    :sparkles: cria tela package view

--- swiftuiandcombine/Views/PackagesView.swift
+++ swiftuiandcombine/Views/PackagesView.swift
@@ -8,8 +8,46 @@
 import SwiftUI
 
 struct PackagesView: View {
+    
+    @State private var contentOffset = CGFloat(0)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .top){
+            TrackableScrollView(offsetChanged: { offsetPoint
+                in
+                contentOffset = offsetPoint.y
+            }) {
+                content
+            }
+            
+            VisualEffectBlur(blurStyle: .systemMaterial)
+                .opacity(contentOffset < -16 ? 1 : 0)
+                .animation(.easeIn)
+                .ignoresSafeArea()
+                .frame(height: 0)
+        }
+        .frame(maxHeight: .infinity, alignment: .top)
+        .navigationTitle("SwiftUI Packages")
+        .background(Color("Background 2"))
+        .edgesIgnoringSafeArea(.all)
+            
+    }
+    
+    var content: some View {
+        VStack(alignment: .leading, spacing: 16){
+            Text("While this version aims to stay true to the original spirit and naming conventions of Rx, this projects also aims to provide a true Swift-first API for Rx APIs.")
+                .font(.subheadline)
+                .opacity(0.7)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .fixedSize(horizontal: false, vertical: true)
+            
+            ForEach(packagesData, id: \.id) {
+                package in
+                PackageRow(package: package)
+            }
+            
+        }.padding(.horizontal, 20)
+        .padding(.top, 160)
     }
 }
 

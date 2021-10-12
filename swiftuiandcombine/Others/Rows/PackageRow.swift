57840229d3e2a46353fc936fbaad06cc05106fc7
Daniel Araujo <danielaraujos@live.com>
2021-10-12 12:42:10 -0300

    :sparkles: cria componente package row

---
+++ swiftuiandcombine/Others/Rows/PackageRow.swift
@@ -0,0 +1,40 @@
+//
+//  PackageRow.swift
+//  swiftuiandcombine
+//
+//  Created by Daniel Araujo on 20/09/21.
+//
+
+import SwiftUI
+
+struct PackageRow: View {
+    
+    @Environment(\.colorScheme) var colorScheme : ColorScheme
+    
+    var package : Package
+    
+    var body: some View {
+        VStack (alignment: .leading) {
+            Link(destination: URL(string: package.link)!, label: {
+                LinearGradient(gradient: Gradient(colors:  colorScheme == .dark ?
+                                                    [Color(#colorLiteral(red: 0.6196078431, green: 0.6784313725, blue: 1, alpha: 1)), Color(#colorLiteral(red: 1, green: 0.5607843137, blue: 0.9803921569, alpha: 1))] : [Color(#colorLiteral(red: 0.09019608051, green: 0, blue: 0.3019607961, alpha: 1))]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                    .frame(height: 20)
+                    .mask(Text(package.title)
+                            .font(.subheadline)
+                            .textCase(.uppercase)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                          
+                            )
+                    
+            })
+            Divider()
+        }
+    }
+}
+
+struct PackageRow_Previews: PreviewProvider {
+    static var previews: some View {
+        PackageRow(package: packagesData[0])
+            .environment(\.colorScheme, .dark)
+    }
+}

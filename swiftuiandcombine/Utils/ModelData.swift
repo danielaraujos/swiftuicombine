33abbb3a0bbb3ab9ec6362ecfe6d5b556f626ec3
Daniel Araujo <danielaraujos@live.com>
2021-10-12 12:43:28 -0300

    :sparkles: cria model data para decodificar o json e transformar em FAQ

---
+++ swiftuiandcombine/Utils/ModelData.swift
@@ -0,0 +1,32 @@
+//
+//  ModelData.swift
+//  swiftuiandcombine
+//
+//  Created by Daniel Araujo on 06/10/21.
+//
+
+import Foundation
+
+var faqDataModel : [FAQ] = load("faqData.json")
+
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("Couldn't find \(filename) in main bundle.")
+    }
+
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
